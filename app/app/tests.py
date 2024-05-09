de6bd83181b9fb4745053328ebd5e85be201ffd4
aakhouaja <aakhouaja@sqli.com>
2024-05-09 23:33:16 +0200

    configure docker-compose to check and wait for db

--- app/app/tests.py
+++ app/app/tests.py
@@ -5,8 +5,8 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
-    
     """test the calc module."""
 
     def test_add_numbers(self):
@@ -19,4 +19,4 @@ class CalcTests(SimpleTestCase):
         res = calc.substract(10, 15)
 
         self.assertEqual(res, 5)
-
+        
--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -2,8 +2,7 @@
 Django command to wait for database to be available
 """
 import time
-from psycopg2 import OperationalError as Psycopg2OpError
-
+from psycopg2 import OperationalError as Psycopg2OpError  # type: ignore
 from django.db.utils import OperationalError 
 from django.core.management.base import BaseCommand
 
@@ -24,3 +23,4 @@ class Command(BaseCommand):
                 time.sleep(1)
 
         self.stdout.write(self.style.SUCCESS('Database available'))
+        
